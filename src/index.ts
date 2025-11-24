3543916644e29c0a912b05bd0219ab25eed03474
24chance <143710541+24chance@users.noreply.github.com>
2025-11-24 10:36:49 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -28,7 +28,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:8000', 'https://e-learning-yixk.onrender.com', 'https://e-learning-backend-v7pk.onrender.com', 'https://e-learning-backend-production-3563.up.railway.app'],
+  origin: ['http://localhost:3000', 'http://localhost:8000', 'https://e-learning-yixk.onrender.com', 'https://e-learning-backend-v7pk.onrender.com', 'https://e-learning-backend-production-3563.up.railway.app', 'https://e-learning-9aud.onrender.com'],
   credentials: true // Enable CORS with credentials
 }));
 app.use(express.json());
